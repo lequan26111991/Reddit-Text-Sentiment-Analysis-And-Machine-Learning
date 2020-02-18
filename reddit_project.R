02a83646d983f1950c85112627c906db0d8d9b19
Richard More <RichardMore@users.noreply.github.com>
2020-02-18 14:57:36 -0500

    Machine learning update
    NRC type counts to individual columns and model predicting the comment score.
    It has a 9.308 RMSE now.

--- reddit_project.R
+++ reddit_project.R
@@ -1,14 +1,17 @@
 #install.packages("RedditExtractoR")
 library(RedditExtractoR)
 library(tidyverse)
-library(tidytext)
 library(wordcloud)
-
+library(tidytext)
+library(recipes)
+library(skimr)
+library(h2o)
 
 #gather data
-r <- reddit_urls(subreddit = "worldnews", page_threshold = 5)
+r <- reddit_urls(subreddit = "Coronavirus", page_threshold = 20)
 rc <- reddit_content(r$URL)
-rc_rel <- rc[c("comment")]
+rc_rel <- rc[c("comment", "comment_score", "post_score")]
+rc_rel <- mutate(rc_rel, id = rownames(rc_rel))
 
 #get tokens from comments
 tokens <- rc_rel %>%
@@ -65,7 +68,7 @@ bing_word_counts <- sent_reviews %>%
   count(word, bing, sort = TRUE)
 
 bing_word_counts %>%
-  filter(n > 100) %>%
+  filter(n > 250) %>%
   mutate(n = ifelse(bing == "negative", -n, n)) %>%
   mutate(word = reorder(word, n)) %>%
   ggplot(aes(word, n, fill = bing)) +  geom_col() +
@@ -78,7 +81,7 @@ nrc_word_counts <- sent_reviews %>%
   count(word, nrc, sort = TRUE)
 
 nrc_word_counts %>%
-  filter(n > 50) %>%
+  filter(n > 250) %>%
   mutate(n = ifelse(nrc %in% c("negative", "fear", "anger", "sadness", "disgust"), -n, n)) %>%
   mutate(word = reorder(word, n)) %>%
   ggplot(aes(word, n, fill = nrc)) +  geom_col() +
@@ -90,3 +93,87 @@ sent_reviews %>%
   filter(!is.na(nrc)) %>%
   ggplot(., aes(x = nrc)) +
   geom_bar()
+
+
+#machine learning
+#################
+
+#id as integer
+sent_reviews <- sent_reviews %>% mutate(id = as.integer(id))
+
+#base df
+df0 <- sent_reviews %>% select("id", "comment_score", "post_score") %>% unique()
+
+#these will be the columns basically
+nrcs <- sent_reviews %>% select("nrc") %>% unique() %>% na.omit() %>% as.list()
+
+for (nrcx in nrcs$nrc) {
+  df1 <- sent_reviews %>%
+    select("id", "nrc") %>%
+    filter(nrc == nrcx) %>%
+    group_by(id) %>%
+    summarise(num = n()) %>%
+    arrange(id)
+  
+  colnames(df1)[2] <- paste0("nrc_", nrcx)
+  
+  df0 <- full_join(df0, df1, by = "id")
+}
+
+x_train_tbl <- df0 %>% select(-c(comment_score, id))
+y_train_tbl <- df0 %>% select(comment_score)
+
+x_train_tbl_skim = partition(skim(x_train_tbl))
+
+rec_obj <- recipe(~ ., data = x_train_tbl) %>%
+  step_meanimpute(all_numeric()) %>% # missing values in numeric columns
+  prep(training = x_train_tbl)
+
+x_train_processed_tbl <- bake(rec_obj, x_train_tbl)
+
+#h2o
+h2o.init(nthreads = -1)
+h2o.clusterInfo()
+h2o.removeAll()
+data_h2o <- as.h2o(
+  bind_cols(y_train_tbl, x_train_processed_tbl),
+  destination_frame= "train.hex"
+)
+
+splits <- h2o.splitFrame(data = data_h2o,
+                         ratios = c(0.7, 0.15), # 70/15/15 split
+                         seed = 1234)
+train_h2o <- splits[[1]]
+valid_h2o <- splits[[2]]
+test_h2o  <- splits[[3]]
+
+y <- "comment_score"
+x <- setdiff(names(train_h2o), y)
+
+hyper_prms <- list(
+  ntrees = c(45, 50, 55),
+  learn_rate = c(0.1, 0.2),
+  max_depth = c(20, 25)
+)
+
+grid_gbm <- h2o.grid(
+  grid_id="grid_gbm",
+  algorithm = "gbm",
+  training_frame = train_h2o,
+  validation_frame = valid_h2o,
+  x = x,
+  y = y,
+  sample_rate = 0.7,
+  col_sample_rate = 0.7,
+  stopping_rounds = 2,
+  stopping_metric = "RMSE",
+  stopping_tolerance = 0.0001,
+  score_each_iteration = T,
+  model_id = "gbm_covType3",
+  balance_classes = T,
+  seed = 5000,
+  hyper_params = hyper_prms
+)
+
+grid <- h2o.getGrid("grid_gbm", sort_by = "rmse", decreasing = FALSE)
+dl_grid_best_model <- h2o.getModel(grid@summary_table$model_ids[1])
