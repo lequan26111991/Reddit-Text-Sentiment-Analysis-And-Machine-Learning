e64dc1ca0faa398604aaa41a8d8823890a37bb84
Quan Le <54324280+lequan26111991@users.noreply.github.com>
2020-02-18 17:05:50 -0500

    Add visualizarion of Corona

--- reddit_project.R
+++ reddit_project.R
@@ -6,6 +6,8 @@ library(tidytext)
 library(recipes)
 library(skimr)
 library(h2o)
+library(readr)
+library(RCurl)
 
 #gather data
 r <- reddit_urls(subreddit = "Coronavirus", page_threshold = 20)
@@ -94,8 +96,78 @@ sent_reviews %>%
   ggplot(., aes(x = nrc)) +
   geom_bar()
 
+#Code to store information of Corona comment overtime on Reddit
+##################################################################################
+# Aggregate Corona Virus
+df_corona <- rc %>% filter(str_detect(title,"corona") | 
+                             str_detect(title,"Corona") |
+                             str_detect(title,"CORONA") |
+                             str_detect(title,"2019-nCOV") |
+                             str_detect(title,"COVID-19") |
+                             str_detect(title,"Covid-19") |
+                             str_detect(title,"covid-19")) %>%
+  select(comm_date) %>%
+  group_by(comm_date) %>% 
+  count(comm_date)
 
-#machine learning
+#change the file Name of Corona
+c_name <- c("Date","Count_Corona")
+colnames(df_corona) <- c_name
+
+# Aggregate World
+df_world <- rc %>% 
+  select(comm_date) %>%
+  group_by(comm_date) %>%
+  count(comm_date)
+
+#change the file Name of World
+w_name <- c("Date","Count_World")
+colnames(df_world) <- w_name
+
+df_export <- inner_join(df_world,df_corona, by ="Date")
+
+if (file.exists("corona_over_time.csv") == TRUE){
+  write.table(df_export,"corona_over_time.csv",
+              append = TRUE, col.names = FALSE, sep =",", row.names=FALSE)
+} else {
+  write.table(df_export,"corona_over_time.csv",
+              append = FALSE, col.names = TRUE, sep =",", row.names=FALSE)
+}
+
+#Code to set Schedule to run the saving Corona information overtime
+#######################################################################################
+#Set schedule everyday to feed data
+
+#install.packages("taskscheduleR")
+#library(taskscheduleR)
+#myscript <- system.file("reddit_corona.R")
+
+#taskscheduler_create(taskname = "CORONA_UPDATE", 
+                     #rscript = "C:/Users/JNK4QBK/Desktop/reddit_corona.R",schedule = "DAILY", 
+                     #starttime = "23:50", startdate = format(Sys.Date()+1, "%d/%m/%Y"))
+
+
+#Visualization of Corona Virus concern overtime based on number of comment
+###########################################################################
+#Retrieve the saving csv file
+urlfile="https://github.com/hnguyen154/RedditBigDataProject/blob/master/corona_over_time.csv"
+
+#Get the csv from Github
+x <- getURL(urlfile)
+df_corona_plot <- read.csv(text = x)
+
+#df_corona_plot <- read.csv("corona_over_time.csv")
+
+#Untidy data for visualization
+df_plot <- df_corona_plot %>%   
+  gather('Count_Corona', 'Count_World', key = "Categories", value = "Total_Comments")
+
+#Plot the Corona Concern
+ggplot(df_plot, aes(fill=Categories, y=Total_Comments, x=Date)) + 
+  geom_bar(position="dodge", stat="identity") +geom_line(aes(x=Date, y=Total_Comments, group=Categories))+
+  geom_point()
+
+#Machine Learning
 #################
 
 #id as integer
